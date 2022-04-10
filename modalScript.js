f55a1b7d052d80ad557b6924446543840c899d51
JakeWLang <JakeWLang@users.noreply.github.com>
2022-04-10 11:18:12 -0500

    Updating personal page

--- modalScript.js
+++ modalScript.js
@@ -3,13 +3,17 @@ let modalImg = document.getElementById('img01');
 let closeSpan = document.getElementsByClassName('close')[0];
 let captionText = document.getElementById('img-caption')
 
-
-
+console.log(modalImg)
 function selImg(d){
 	// console.log(d)
 	console.log(d.getAttribute('src'));
 	modal.style.display = 'block';
-	modalImg.src = d.getAttribute('src');
+
+	if (d.getAttribute('src') == null) {
+		modalImg.src = 'Portfolio_Images/IMG_6099.jpeg'
+	} else {
+		modalImg.src = d.getAttribute('src');
+	}
 	modalImg.style.backgroundColor = 'white'
 	captionText.innerHTML = d.getAttribute('alt')
 }
