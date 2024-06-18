669d36524245a8686a268425c522f4cf2ed3ee35
JakeWLang <JakeWLang@users.noreply.github.com>
2024-06-18 18:40:41 -0500

    add modals and fix svgs

--- modalScript.js
+++ modalScript.js
@@ -1,24 +1,24 @@
-// let modal = document.getElementById('photobox');
-// let modalImg = document.getElementById('img01');
-// let closeSpan = document.getElementsByClassName('close')[0];
-// let captionText = document.getElementById('img-caption')
+let modal = document.getElementById('photobox');
+let modalImg = document.getElementById('img01');
+let closeSpan = document.getElementsByClassName('close')[0];
+let captionText = document.getElementById('img-caption')
 
-// console.log(modalImg)
-// function selImg(d){
-// 	// console.log(d)
-// 	console.log(d.getAttribute('src'));
-// 	modal.style.display = 'block';
+console.log(modalImg)
+function selImg(d){
+	// console.log(d)
+	console.log(d.getAttribute('src'));
+	modal.style.display = 'flex';
 
-// 	if (d.getAttribute('src') == null) {
-// 		modalImg.src = 'Portfolio_Images/IMG_6099.jpeg'
-// 	} else {
-// 		modalImg.src = d.getAttribute('src');
-// 	}
-// 	modalImg.style.backgroundColor = 'white'
-// 	captionText.innerHTML = d.getAttribute('alt')
-// }
+	if (d.getAttribute('src') == null) {
+		modalImg.src = '/innovation/images/fake-discount.png'
+	} else {
+		modalImg.src = d.getAttribute('src');
+	}
+	modalImg.style.backgroundColor = 'white'
+	captionText.innerHTML = d.getAttribute('alt')
+}
 
-// closeSpan.onclick = function() {
-// 	modal.style.display = 'none';
-// }
+closeSpan.onclick = function() {
+	modal.style.display = 'none';
+}
 
