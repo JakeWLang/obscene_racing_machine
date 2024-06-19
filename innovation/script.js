731a596ca4ec878393a8744263082d0217282e88
JakeWLang <JakeWLang@users.noreply.github.com>
2024-06-18 19:08:12 -0500

    make lightStar work

--- innovation/script.js
+++ innovation/script.js
@@ -123,3 +123,44 @@ if (title === 'TAP - Rewards') {
     let pct = (pts / 1000) * 100
     progBar(pct)
 }
+
+
+function lightStar(star) {
+    for (let i = 0; i < 5; i++) {
+        document.getElementById('star' + i).setAttribute('fill', 'none')
+    }
+    let id = star.id
+    let priorStars = Math.abs(0 - id[id.length - 1])
+    let starsToLight = range([0, priorStars + 1])
+    for (let i = 0; i < starsToLight.length; i++) {
+        let thisStar = document.getElementById('star' + i)
+        console.log(thisStar)
+        thisStar.setAttribute('fill', 'green')
+    }
+}
+
+
+// let modal = document.getElementById('photobox');
+// let modalImg = document.getElementById('img01');
+// let closeSpan = document.getElementsByClassName('close')[0];
+// let captionText = document.getElementById('img-caption')
+
+// console.log(modalImg)
+// function selImg(d){
+// 	// console.log(d)
+// 	console.log(d.getAttribute('src'));
+// 	modal.style.display = 'flex';
+
+// 	if (d.getAttribute('src') == null) {
+// 		modalImg.src = '/innovation/images/fake-discount.png'
+// 	} else {
+// 		modalImg.src = d.getAttribute('src');
+// 	}
+// 	modalImg.style.backgroundColor = 'white'
+// 	captionText.innerHTML = d.getAttribute('alt')
+// }
+
+// closeSpan.onclick = function() {
+// 	modal.style.display = 'none';
+// }
+
