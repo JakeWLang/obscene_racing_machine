2f36ceca1d6fcd0037fd852469d6427924e221be
JakeWLang <JakeWLang@users.noreply.github.com>
2024-07-14 17:23:43 -0500

    change star coloring and add line to forms

--- innovation/script.js
+++ innovation/script.js
@@ -165,16 +165,83 @@ function progBar(pct) {
 }
 
 
+getGradientColor = function(start_color, end_color, percent) {
+    // strip the leading # if it's there
+    start_color = start_color.replace(/^\s*#|\s*$/g, '');
+    end_color = end_color.replace(/^\s*#|\s*$/g, '');
+  
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if (start_color.length == 3) {
+      start_color = start_color.replace(/(.)/g, '$1$1');
+    }
+  
+    if (end_color.length == 3) {
+      end_color = end_color.replace(/(.)/g, '$1$1');
+    }
+  
+    // get colors
+    var start_red = parseInt(start_color.substr(0, 2), 16),
+      start_green = parseInt(start_color.substr(2, 2), 16),
+      start_blue = parseInt(start_color.substr(4, 2), 16);
+  
+    var end_red = parseInt(end_color.substr(0, 2), 16),
+      end_green = parseInt(end_color.substr(2, 2), 16),
+      end_blue = parseInt(end_color.substr(4, 2), 16);
+  
+    // calculate new color
+    var diff_red = end_red - start_red;
+    var diff_green = end_green - start_green;
+    var diff_blue = end_blue - start_blue;
+  
+    diff_red = ((diff_red * percent) + start_red).toString(16).split('.')[0];
+    diff_green = ((diff_green * percent) + start_green).toString(16).split('.')[0];
+    diff_blue = ((diff_blue * percent) + start_blue).toString(16).split('.')[0];
+  
+    // ensure 2 digits by color
+    if (diff_red.length == 1) diff_red = '0' + diff_red
+    if (diff_green.length == 1) diff_green = '0' + diff_green
+    if (diff_blue.length == 1) diff_blue = '0' + diff_blue
+  
+    return '#' + diff_red + diff_green + diff_blue;
+}
+
+function makeAMix(num, denom, aColor='#F1A45F', bColor='#7EF15F') {
+    let mix = num / denom
+    if (mix == 1 / denom) {
+        mix = 0
+    }
+    let color = getGradientColor(aColor, bColor, mix)
+    return color
+}
+
+// function mouseStar(star) {
+//     let id = star.id[star.id.length - 1]
+//     let color = makeAMix(id, 4)
+//     for (let i = 0; i <= id; i++) {
+//         document.getElementById('star' + i).setAttribute('fill', color)
+//     }
+//     // document.getElementById(star.id).setAttribute('fill', color)
+// }
+
+// function mouseOffStar(star) {
+//     let id = star.id[star.id.length - 1]
+//     for (let i = 0; i <= id; i++) {
+//         document.getElementById('star' + i).setAttribute('fill', 'none')
+//     }
+// }
+
+
 function lightStar(star) {
     for (let i = 0; i < 5; i++) {
         document.getElementById('star' + i).setAttribute('fill', 'none')
     }
     let id = star.id
+    let color = makeAMix(id[id.length-1], 5)
     let priorStars = Math.abs(0 - id[id.length - 1])
     let starsToLight = range([0, priorStars + 1])
     for (let i = 0; i < starsToLight.length; i++) {
         let thisStar = document.getElementById('star' + i)
-        thisStar.setAttribute('fill', 'green')
+        thisStar.setAttribute('fill', color)
     }
 }
 
