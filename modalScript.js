576204bd585ad9561cb6e014cd4d385bd81a6d62
JakeWLang <JakeWLang@users.noreply.github.com>
2024-07-20 07:25:12 -0500

    add reward images. try change favicon

--- modalScript.js
+++ modalScript.js
@@ -8,12 +8,28 @@ function selImg(d){
 	// console.log(d)
 	console.log(d.getAttribute('src'));
 	modal.style.display = 'flex';
-
-	if (d.getAttribute('src') == null) {
-		modalImg.src = '/innovation/images/fake-discount.png'
-	} else {
-		modalImg.src = d.getAttribute('src');
+	let src = ''
+	if (d.getAttribute('id') == 'giftcard') {
+		src = '/innovation/images/toy-train.png'
 	}
+	else if (d.getAttribute('id') == 'oneday') {
+		src = '/innovation/images/fun_pass.jpg'
+	}
+	else if (d.getAttribute('id') == 'field') {
+		src = '/innovation/images/sue.jpg'
+	}
+	else if (d.getAttribute('id') == 'raffle') {
+		src = '/innovation/images/tickies.webp'
+	}
+	else if (d.getAttribute('id') == 'thirtyday') {
+		src = '/innovation/images/30_day_pass.jpg'
+	}
+	else if (d.getAttribute('src') == null) {
+		src = '/innovation/images/fake-discount.png'
+	} else {
+		src = d.getAttribute('src');
+	}
+	modalImg.src = src
 	modalImg.style.backgroundColor = 'white'
 	captionText.innerHTML = d.getAttribute('alt')
 }
